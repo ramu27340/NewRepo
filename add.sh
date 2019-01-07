ef598db6b66122a15dbcd32ba3a719830f97799f
ramu <ramu@gmail.com>
2019-01-07 17:45:23 +0530

    update 2 add

--- add.sh
+++ add.sh
@@ -3,7 +3,7 @@ groupadd student;
 for i in `more userlist.txt` 
 do
 adduser -g student $i;
-echo "user $i added successfully created"
+echo "user $i added successfully created";
 echo ""
 echo $i"123"|passwd --stdin $i
 echo;
